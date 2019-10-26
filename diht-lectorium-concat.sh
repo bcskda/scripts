c842d581ccdf2a3e56c33834bdf4dfc9247caade
bcskda <demulkup@ya.ru>
2019-10-26 08:35:08 +0300

    diht-lectorium-concat: cleaner code

--- diht-lectorium-concat.sh
+++ diht-lectorium-concat.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 pwd
-ffmpeg -i "concat:$(echo *.${1:-MTS} | sed -e 's/ /|/g' | tail -c +1 -)" -c:v copy -c:a aac -ab 384K "${2:-source.mp4}"
+files="$(echo *.${1:-MTS} | sed -e 's/ /|/g' | tail -c +1 -)"
+options="-c:v copy -c:a ac3 -ab 384K"
+ffmpeg -i "concat:$files" $options "${2:-source_concat.mp4}"
