ff38fe1b98949bed362c3febf2828420590544d9
bcskda <demulkup@ya.ru>
2019-10-26 08:37:16 +0300

    diht-lectorium-compress: concat + compress ~4x

---
+++ diht-lectorium-compress.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pwd
+files="$(echo *.${1:-MTS} | sed -e 's/ /|/g' | tail -c +1 -)"
+options="-c:v libx264 -crf 23 -c:a ac3 -ab 384K"
+ffmpeg -i "concat:$files" $options "${2:-source_compress.mp4}"
