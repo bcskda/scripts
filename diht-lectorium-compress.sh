661aa4f2d129355609d3fb6ff87866961a048b7a
bcskda <demulkup@ya.ru>
2019-11-08 18:20:04 +0300

    lectorium: mp3

--- diht-lectorium-compress.sh
+++ diht-lectorium-compress.sh
@@ -2,5 +2,5 @@
 
 pwd
 files="$(echo *.${1:-MTS} | sed -e 's/ /|/g' | tail -c +1 -)"
-options="-c:v libx264 -crf 23 -c:a ac3 -ab 384K"
+options="-c:v libx264 -crf 23 -c:a mp3 -ab 384K"
 ffmpeg -i "concat:$files" $options "${2:-source_compress.mp4}"
--- diht-lectorium-concat.sh
+++ diht-lectorium-concat.sh
@@ -2,5 +2,5 @@
 
 pwd
 files="$(echo *.${1:-MTS} | sed -e 's/ /|/g' | tail -c +1 -)"
-options="-c:v copy -c:a ac3 -ab 384K"
+options="-c:v copy -c:a mp3 -ab 384K"
 ffmpeg -i "concat:$files" $options "${2:-source_concat.mp4}"
