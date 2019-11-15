0900810d0f5d43911191f3a202ecef13f9f751ef
bcskda <demulkup@ya.ru>
2019-11-15 22:52:24 +0300

    diht-lectorium-compess: produce mp3+aac audio

--- diht-lectorium-compress.sh
+++ diht-lectorium-compress.sh
@@ -2,5 +2,7 @@
 
 pwd
 files="$(echo *.${1:-MTS} | sed -e 's/ /|/g' | tail -c +1 -)"
-options="-c:v libx264 -crf 23 -c:a mp3 -ab 384K"
+options_map="-map 0:0 -map 0:1 -map 0:1"
+options_codec="-c:v libx264 -crf 23 -c:a:0 mp3 -ab 384K -c:a:1 aac -ab 384K"
+options="$options_map $options_codec"
 ffmpeg -i "concat:$files" $options "${2:-source_compress.mp4}"
