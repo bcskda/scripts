c219effe7d2567a2c46e6f455b2d76b3aeb21667
bcskda <demulkup@ya.ru>
2020-02-10 12:43:52 +0300

    proxy-check.py: write_graphite

--- proxy_check.py
+++ proxy_check.py
@@ -1,27 +1,58 @@
+import time
 import os
 import requests
+import socket
+from sched import scheduler
 
 
-def is_reachable(url, proxy) -> bool:
-    success = False
+ENCODING = 'utf-8'
+INTERVAL_SEC = 60
+
+def check_status_code(url, proxy) -> bool:
+    code = 0
     try:
         resp = requests.head(url, proxies={"https": proxy})
-        success = resp.status_code == requests.status_codes.ok
+        code = resp.status_code
     except Exception as e:
-        print(e)
-    return success
+        pass
+    return code
+
+def write_graphite(graphite_addr, key, value, ts=None):
+    if ' ' in key:
+        raise ValueError("Space in metric name")
+    if ts is None:
+        ts = int(time.time())
+    mesg = ' '.join(map(str, (key, value, ts))) + '\n'
+    print(mesg)
+    with socket.create_connection(graphite_addr) as sock:
+        sock.sendall(mesg.encode(ENCODING))
+        sock.shutdown(socket.SHUT_RDWR)
+
+def get_args():
+    env_keys = [
+        "https_proxy", "https_target", "metric_key", "graphite_host",
+        "graphite_port"
+    ]
+    env_values = tuple(map(os.getenv, env_keys))
+    if not all(env_values):
+        raise ValueError(f"one or more environment variables missing: {env_keys}")
+    return env_values
+
+def report_single(proxy, target, metric, graphite_addr):
+    value = check_status_code(target, proxy)
+    write_graphite(graphite_addr, metric, value)
 
 def main():
-    proxy = os.getenv("https_proxy")
-    target = os.getenv("https_target")
-    if not all((proxy, target)):
-        print("one or more environment variables missing: https_proxy, https_target")
-        return
-    
-    if is_reachable(target, proxy):
-        print("OK")
-    else:
-        print("Error")
+    proxy, target, metric, host, port = get_args()
+    graphite_addr = (host, port)
+    sched = scheduler()
+    while True:
+        try:
+            sched.enter(INTERVAL_SEC, 1, report_single,
+                argument=(proxy, target, metric, graphite_addr))
+            sched.run()
+        except KeyboardInterrupt:
+            break
 
 if __name__ == "__main__":
     main()
